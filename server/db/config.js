7334dbd5dc8f60bc13a49a50d3bd4046b20f707d
Swaroop Dora <105218991+automatefreely@users.noreply.github.com>
2023-07-31 23:40:25 +0530

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
 const MongoURL = "mongodb+srv://ekanshbhushan:PC6uj362iYe0zu6X@cluster0.z6hljps.mongodb.net/?retryWrites=true&w=majority";
-const DB = "skillop-db"
-mongoose.connect(`${MongoURL}/${DB}`);
+// const DB = "skillop-db"
+// mongoose.connect(`${MongoURL}/${DB}`);
+mongoose.connect(`${MongoURL}`);
